b2fe881c37ce58eea1144edf95c7fd6be3e03c1c
MikMosk86 <mimoal1986@mail.ru>
2026-03-08 15:59:19 +0300

    добавил оптиму

--- visit_calculator.py
+++ visit_calculator.py
@@ -319,7 +319,11 @@ class VisitCalculator:
                 visits_df['Дата визита'] = visits_df['Дата визита'].dt.normalize()
             
             # ФИЛЬТРЫ
-            completed_mask = visits_df[status_col].isin(['Выполнено', 'выполнен'])
+            completed_mask = visits_df[status_col].isin([
+                'Выполнено', 'выполнен',      # существующие
+                'Заполнена',     # новое для Optima
+                'Проверена'      # новое для Optima
+            ])
             start_date = pd.Timestamp(calc_params['start_date'])
             end_date = pd.Timestamp(calc_params['end_date'])
             period_mask = (
@@ -455,64 +459,6 @@ class VisitCalculator:
 
 # Глобальный экземпляр
 visit_calculator = VisitCalculator()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
